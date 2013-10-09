4d31e5d20b062f7f6dbd74ff075c2b5221b06aa7
Niccoli <nhersey@gmail.com>
2013-10-09 10:21:54 -0700

    Initial commit. User, Book & Category tables and models created.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Iread::Application.config.secret_key_base = '0fe9310501406e5872a10ff80764b5a2c2b11dd3c2185b4fcfc3e5e6403d275457e90de26f5e813a90b72e0569638e0f0e6f4e90d86c278f9769412d4466b0f3'
